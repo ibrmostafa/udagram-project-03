de06b8bc025212a5c2069923e21317a76c57bb0d
Mostafa Ibrahim A <mostafa.a.ibrahim@ericsson.com>
2022-10-02 17:27:57 +0200

    First Mirco Service code

---
+++ set_env.sh
@@ -0,0 +1,11 @@
+# This file is used for convenience of local development.
+# DO NOT STORE YOUR CREDENTIALS INTO GIT
+export POSTGRES_USERNAME=udacity
+export POSTGRES_PASSWORD=udacity12345
+export POSTGRES_HOST=project-03.cgcx0y5aug5w.us-east-1.rds.amazonaws.com
+export POSTGRES_DB=udacity
+export AWS_BUCKET=arn:aws:s3:::udacity-project-03
+export AWS_REGION=us-east-1
+export AWS_PROFILE=default
+export JWT_SECRET=testing
+export URL=http://localhost:8100
